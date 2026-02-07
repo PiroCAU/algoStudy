4dbd57274cd644e0ed533174d38d145bcd03c749
wldy4627 <wldy4627@gmail.com>
2026-02-08 00:11:48 +0900

    32week: BOJ[2075] 우선순위 큐

---
+++ BOJ/[2075] N번째 큰 수/wldy4627/Main.java
@@ -0,0 +1,35 @@
+package boj.b2075.wldy4627;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < N; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                if (pq.size() < N) {
+                    pq.offer(num);
+                } else {
+                    int min = pq.peek();
+                    if (min < num) {
+                        pq.poll();
+                        pq.offer(num);
+                    }
+                }
+            }
+        }
+
+        System.out.println(pq.poll());
+    }
+}
